c1e45edd159dd1ef8a837b56eae9bc630c8233ae
TetieWasTaken <tjvssr@gmail.com>
2025-03-04 17:57:52 +0100

    feat: logtape

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import { createFederation, MemoryKvStore } from "@fedify/fedify";
 import { serve } from "@hono/node-server";
+import { configure, getConsoleSink } from "@logtape/logtape";
+
+await configure({
+  sinks: { console: getConsoleSink() },
+  filters: {},
+  loggers: [
+    { category: "fedify", sinks: ["console"], lowestLevel: "info" },
+  ],
+});
 
 const federation = createFederation<void>({
   kv: new MemoryKvStore(),
