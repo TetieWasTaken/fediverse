917f7e2e88f12f57032d4a26ef013327061e58ed
TetieWasTaken <tjvssr@gmail.com>
2025-03-08 12:08:12 +0100

    feat: inbox

--- server.ts
+++ server.ts
@@ -1,51 +1,47 @@
-import { createFederation, MemoryKvStore, Person } from "@fedify/fedify";
+import { createFederation, MemoryKvStore, Person, Follow, exportJwk, generateCryptoKeyPair, importJwk } from "@fedify/fedify";
 import { serve } from "@hono/node-server";
 import { configure, getConsoleSink } from "@logtape/logtape";
+import ngrok from "@ngrok/ngrok";
 
 await configure({
   sinks: { console: getConsoleSink() },
   filters: {},
-  loggers: [
-    { category: "fedify", sinks: ["console"], lowestLevel: "info" },
-  ],
+  loggers: [{ category: "fedify", sinks: ["console"], lowestLevel: "info" }],
 });
 
 const federation = createFederation<void>({
   kv: new MemoryKvStore(),
 });
 
-federation
-  .setActorDispatcher
-  ("/users/{identifier}", async (ctx
-    , identifier
-  ) => {
-    if (identifier
-      !== "me") return null;  // Other than "me" is not found.
-    return new Person
-      ({
-        id
-          : ctx
-            .getActorUri
-            (identifier
-            ),
-        name
-          : "Me",  // Display name
-        summary
-          : "This is me!",  // Bio
-        preferredUsername
-          : identifier
-        ,  // Bare handle
-        url
-          : new URL
-            ("/", ctx
-              .url
-            ),
-      });
+federation.setInboxListeners("/users/{identifier}/inbox", "/inbox").on(Follow, async (ctx, follow) => {
+  if (follow.id == null || follow.actorId == null || follow.objectId == null) {
+    return;
+  }
+  const parsed = ctx.parseUri(follow.objectId);
+  if (parsed?.type !== "actor" || parsed.identifier !== "me") return;
+  const follower = await follow.getActor(ctx);
+  console.debug(follower);
+});
+
+federation.setActorDispatcher("/users/{identifier}", async (ctx, identifier) => {
+  if (identifier !== "me") return null; // Other than "me" is not found.
+  return new Person({
+    id: ctx.getActorUri(identifier),
+    name: "Me", // Display name
+    summary: "This is me!", // Bio
+    preferredUsername: identifier, // Bare handle
+    url: new URL("/", ctx.url),
+    inbox: ctx.getInboxUri(identifier),
   });
+});
 
 serve({
-  port: 8000,
+  port: 8080,
   fetch(request) {
     return federation.fetch(request, { contextData: undefined });
-  }
+  },
+});
+
+await ngrok.connect({ addr: 8080, authtoken_from_env: true }).then((listener) => {
+  console.log(`Server running at ${listener.url()}`);
 });
